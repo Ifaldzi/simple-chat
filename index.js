89cf9397f2b7ba49b7f3c21eaa553bbdaaf7f3a1
Ifaldzi <ifaldzialwihudhori@gmail.com>
2021-08-25 17:30:08 +0700

    deploy to heroku

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require('socket.io');
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+let users = {};
+io.on('connection', (socket) => {
+    socket.on('enter room', (nickname) => {
+        users[socket.id] = nickname;
+        msg = nickname + ' joined the room';
+        socket.broadcast.emit('user connected', msg, {userId: socket.id, nickname: nickname});
+        socket.emit('users', users);
+    });
+
+    socket.on('private message', ({message, to}) => {
+        console.log(message, to);
+        socket.to(to).emit('private message', {
+            message,
+            from: socket.id
+        });
+    })
+
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', users[socket.id] + ': ' + msg);
+    });
+
+    socket.on('disconnect', () => {
+        socket.broadcast.emit('user disconnected', users[socket.id] + ' left the room');
+
+        delete users[socket.id];
+        io.emit('update online users', socket.id);
+    });
+
+    socket.on('typing', () => {
+        socket.broadcast.emit('typing', users[socket.id] + "'s typing");
+    });
+
+    socket.on('stop typing', () => {
+        socket.broadcast.emit('stop typing');
+    })
+});
+
+server.listen(3000, () => {
+    console.log('listening on *:3000');
+})
